2e2b38a5244763b31b102f8c2af613cc0e904c19
KKardd <wlstjd4013@gmail.com>
2024-09-20 07:09:54 +0900

    feat: declare prisma service

---
+++ src/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query'>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [{ emit: 'stdout', level: 'query' }],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
