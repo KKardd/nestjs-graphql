2c1429869dc88ed555d07613a6de1cac66d267ef
KKardd <wlstjd4013@gmail.com>
2024-09-20 07:11:13 +0900

    feat: declare user model

---
+++ src/users/user.model.ts
@@ -0,0 +1,14 @@
+// TODO Apply Nexus?
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+}
