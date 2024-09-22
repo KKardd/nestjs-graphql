a6ca3e46b973648bd02bb9bb7de566d5fc19289a
KKardd <wlstjd4013@gmail.com>
2024-09-22 22:29:28 +0900

    feat: define post model type

---
+++ src/posts/posts.model.ts
@@ -0,0 +1,17 @@
+// TODO Apply Nexus?
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Post {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  title: string;
+
+  @Field()
+  content: string;
+
+  @Field()
+  published: boolean;
+}
