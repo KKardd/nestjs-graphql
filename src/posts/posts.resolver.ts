3e93913aeb644c8f94b5f42d6aba502cefd7d75c
KKardd <wlstjd4013@gmail.com>
2024-09-22 22:30:56 +0900

    feat: add resolver in post methods

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,4 +1,42 @@
-import { Resolver } from '@nestjs/graphql';
+// src/post/post.resolver.ts
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { PostsService } from './posts.service';
 
-@Resolver()
-export class PostsResolver {}
+@Resolver('Post')
+export class PostsResolver {
+  constructor(private postService: PostsService) {}
+
+  @Query('posts')
+  getPosts() {
+    return this.postService.findAll();
+  }
+
+  @Query('post')
+  getPost(@Args('id') id: number) {
+    return this.postService.findOne(id);
+  }
+
+  @Mutation('createPost')
+  createPost(
+    @Args('title') title: string,
+    @Args('content') content: string,
+    @Args('published') published: boolean,
+  ) {
+    return this.postService.createPost({ title, content, published });
+  }
+
+  @Mutation('updatePost')
+  updatePost(
+    @Args('id') id: number,
+    @Args('title') title: string,
+    @Args('content') content: string,
+    @Args('published') published: boolean,
+  ) {
+    return this.postService.updatePost({ id, title, content, published });
+  }
+
+  @Mutation('deletePost')
+  deletePost(@Args('id') id: number) {
+    return this.postService.deletePost(id);
+  }
+}
