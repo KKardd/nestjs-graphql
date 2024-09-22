5cd88555468d71440efac51b3da4762c9fc3983e
KKardd <wlstjd4013@gmail.com>
2024-09-22 22:29:58 +0900

    feat: DI prisma service in post module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [PostsService, PostsResolver]
+  providers: [PostsService, PostsResolver, PrismaService],
 })
 export class PostsModule {}
