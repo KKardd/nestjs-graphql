3f4ea7821fe74e537150bb9417a136b0d64322fe
KKardd <wlstjd4013@gmail.com>
2024-09-26 02:47:40 +0900

    feat: add nexus method in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,35 @@ import { join } from 'path';
 import { PrismaService } from './prisma.service';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CommentsModule } from './comments/comments.module';
+import { makeSchema } from 'nexus';
+import { User } from './users/user.model';
+import { Post } from './posts/posts.model';
+import { Comment } from './comments/comment.model';
 
 @Module({
   imports: [
     UsersModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      // Code First
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 자동으로 스키마 생성
-      // posts module은 Schema First로 구현해보기
-      typePaths: ['./**/*.gql'],
+
+      // Schema First
+      // typePaths: ['./**/*.gql'],
+
+      // Nexus
+      schema: makeSchema({
+        types: [User, Post, Comment],
+        outputs: {
+          typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+          schema: join(__dirname, '..', 'schema.graphql'),
+        },
+        contextType: {
+          module: join(__dirname, './context.js'),
+          export: 'Context',
+        },
+      }),
+
       installSubscriptionHandlers: true,
     }),
     CommentsModule,
